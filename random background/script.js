b44d1a2e20a1055aec2cd8b44bb77620e6f32d1d
Sergey <37954897+klevtcov@users.noreply.github.com>
2020-12-19 14:53:48 +0300

    hex added

--- random background/script.js
+++ random background/script.js
@@ -1,35 +1,34 @@
+var btn1 = document.getElementById('btn1');
+var btn2 = document.getElementById('btn2');
+var main = document.querySelector('.main');
 
-var random6 = function() {
-    let ColoredLetters = ['a', 'b', 'c', 'd', 'e', 'f'];
+var randomHex = function () {
+    let coloredLetters = ['a', 'b', 'c', 'd', 'e', 'f'];
     let i = parseInt(Math.random() * 16);
     if (i < 10) {
-        return i; 
+        return i;
     } else {
-       return ColoredLetters[(i - 10)];
+        return coloredLetters[(i - 10)];
     }
-}
+};
 
-var randomColor = function() {
+var randomColor = function () {
     let colorForRandom = '#';
     for (let i = 0; i < 6; i++) {
-        colorForRandom += random6();
+        colorForRandom += randomHex();
     }
     return colorForRandom;
-}
+};
 
-var inputGradient = function() {
-    document.querySelector('.gradient').style.backgroundImage = `linear-gradient(to top, ${randomColor()} 0%, ${randomColor()} 100%)`;
-}
+var inputGradient = () => document.querySelector('.gradient').style.backgroundImage = `linear-gradient(to top, ${randomColor()} 0%, ${randomColor()} 100%)`;
 
-document.getElementById('btn1').addEventListener('click', () => {
-    inputGradient(); 
-});
+btn1.addEventListener('click', inputGradient);
 
-document.getElementById('btn2').addEventListener('click', () => {
-    document.getElementById('btn2').classList.toggle('clicked-btn');
-    if (document.getElementById('btn2').classList.contains('clicked-btn')) {
-        document.querySelector('.main').setAttribute('onmousemove', 'inputGradient()');
+btn2.addEventListener('click', () => {
+    btn2.classList.toggle('clicked-btn');
+    if (btn2.classList.contains('clicked-btn')) {
+        main.setAttribute('onmousemove', 'inputGradient()');
     } else {
-        document.querySelector('.main').removeAttribute('onmousemove');
+        main.removeAttribute('onmousemove');
     };
 });
